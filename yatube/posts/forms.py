a53323843010e9676123e3f60219aebde8fc7490
Гут Владимир <vladimir.monolith@yandex.ru>
2022-04-18 14:57:38 +0300

    Финальная версия

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -5,6 +5,14 @@ from .models import Comment, Post
 
 class PostForm(forms.ModelForm):
     """Класс для создания формы записи."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs['placeholder'] = (
+            'Здесь Вы можете оставить Вашу запись'
+        )
+        self.fields['group'].empty_label = (
+            'Здесь Вы можете выбрать сообщество'
+        )
 
     class Meta:
         model = Post
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'core.apps.CoreConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -29,6 +30,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -111,3 +113,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache', }}
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
